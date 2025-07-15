a0a95ea5c4724e705e0e9d8da4f78cb7a23ce2e3
jmsangro <jmsangro@gmail.com>
2025-07-15 12:19:10 -0600

    control which screen on which to launch app via -Dscreen command line property

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module BmccStateWallMosaic {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens mosaic to javafx.graphics, javafx.fxml;
 }
--- src/mosaic/Main.java
+++ src/mosaic/Main.java
@@ -1,8 +1,10 @@
 package mosaic;
 	
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
@@ -11,26 +13,35 @@ import javafx.geometry.Rectangle2D;
 
 public class Main extends Application {
 	public static ImageSource imageSource = null;
-	public static String stateName;
 	public static final double aspectRatio = 1080.0/1920.0 ;
 	@Override
 	public void start(Stage primaryStage) {
 		
 		try {
 		    String imagesDir = System.getProperty("imagesDir");
-		    stateName = System.getProperty("stateName");
+		    String screenStr = System.getProperty("screen");
+		    int screenIndex = ((screenStr == null)? 0 : Integer.parseInt(screenStr));
 			imageSource = new DirTreeImageSource(imagesDir);
 			Parent root = (Parent)FXMLLoader.load(getClass().getResource("Mosaic.fxml"));
 			//maximize on screen
-			Rectangle2D screenBounds = Screen.getPrimary().getBounds();
-			double width = screenBounds.getHeight() * aspectRatio;
-			width = screenBounds.getWidth();
+			ObservableList<Screen> screenList = Screen.getScreens();
+			Screen targetScreen = Screen.getPrimary();
+			if (screenIndex < screenList.size()) {
+				targetScreen = screenList.get(screenIndex);
+			}
+			
+			Rectangle2D screenBounds = targetScreen.getVisualBounds();
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			 primaryStage.setX(screenBounds.getMinX());
+			 primaryStage.setY(screenBounds.getMinY());
+			 primaryStage.setWidth(screenBounds.getWidth());
+			 primaryStage.setHeight(screenBounds.getHeight());
 		    String styleSheet = System.getProperty("styleSheet");
 		    if (styleSheet == null) {
 		    	System.out.println("WARNING: No stylesheet specified with -DstyleSheet command line option. Reverting to application.css");
 		    	styleSheet = "application.css";
 		    }
-			Scene scene = new Scene(root,width,screenBounds.getHeight());
+			Scene scene = new Scene(root,screenBounds.getWidth(),screenBounds.getHeight());
 			scene.getStylesheets().add(getClass().getResource(styleSheet).toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setFullScreen(true);
